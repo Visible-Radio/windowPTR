0714152f257ccbdbb9b0478ce56056fff79ac736
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2023-09-06 21:15:51 -0400

    ready for npm package

--- src/devHarness.ts
+++ src/devHarness.ts
@@ -1,4 +1,31 @@
+import { createPTR } from "./lib/createPTR";
 import { neuromancerText } from "./sampleText/neuromancer";
+
+const PTR = createPTR(document.getElementById("root") as HTMLDivElement, {
+  scale: 2,
+});
+
+declare global {
+  interface Window {
+    _PTR: typeof PTR;
+  }
+}
+
+window._PTR = PTR;
+
+const borgText = `
+<span>
+  <span outline=true>
+    Resistance is futile
+  </span>
+  <p highlight=true>
+    Resistance is futile
+  </p>
+  <p>We are the Borg. Lower your shields and surrender your ships. We will add your biological and technological distinctiveness to our own. Your culture will adapt to service us.</p>
+</span>
+`;
+
+PTR.actions.setText(borgText);
 
 const buttons = [
   {
