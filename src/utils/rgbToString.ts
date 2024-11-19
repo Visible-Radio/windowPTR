ff1badac836ef764510cd993d1551c78ce08ce9c
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2024-11-19 09:14:28 -0500

    feat(i-29): tweak cursor

--- src/utils/rgbToString.ts
+++ src/utils/rgbToString.ts
@@ -1,6 +1,6 @@
-import { int8Bit } from "./typeUtils/intRange";
+import { int8Bit } from './typeUtils/intRange';
 
-export function rgbToString(rgbArr: [int8Bit, int8Bit, int8Bit]) {
+export function rgbToString(rgbArr: [int8Bit, int8Bit, int8Bit], alpha = 1) {
   const [r, g, b] = rgbArr;
-  return `rgb(${r},${g},${b})`;
+  return `rgba(${r},${g},${b},${alpha})`;
 }
