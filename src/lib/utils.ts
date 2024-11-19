b376f5004f63836505921441a0f3d480977f856d
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2024-11-18 22:01:37 -0500

    feat(i-29): clicking roughed in

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,14 +10,15 @@ export function compose(...functions) {
   return (...args) => functions.reduceRight((acc, fn) => fn(acc), ...args);
 }
 
-export function generateRandomColors() {
-  const random = () => {
-    const num = Math.floor(Math.random() * (350 - 50) + 50);
-    return num > 255 ? 255 : num;
-  };
-  const R = random();
-  const G = random();
-  const B = random();
-  const color = `rgb(${R},${G},${B})`;
+const random8Bit = () => {
+  const num = Math.floor(Math.random() * (350 - 50) + 50);
+  return num > 255 ? 255 : num;
+};
+
+export function generateRandomColors(alpha = 1) {
+  const R = random8Bit();
+  const G = random8Bit();
+  const B = random8Bit();
+  const color = `rgba(${R},${G},${B},${alpha})`;
   return color;
 }
